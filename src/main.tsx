574635af3fce022f7dcc68cb1c558c0f39b235db
Eder Sampaio <edersampaio@outlook.com.br>
2023-02-26 11:48:36 -0300

    feat: add providers

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from 'react-query'
+import App from './App'
+
+const queryClient = new QueryClient()
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <QueryClientProvider client={queryClient}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </QueryClientProvider>
+)
