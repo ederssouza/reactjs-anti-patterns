7ec140b3e4db1157a9ddafc3c90eeb33b26f0316
Eder Sampaio <edersampaio@outlook.com.br>
2023-02-26 11:48:01 -0300

    feat: add router

---
+++ src/App.tsx
@@ -0,0 +1,57 @@
+import { Routes, Route } from 'react-router-dom'
+import { Layout } from './components'
+import {
+  ComponentWithinComponentsPage,
+  HomePage,
+  InvokingComponentFunctionsDirectlyPage,
+  MultipleStatesPage,
+  NoMatchPage,
+  AbortingFetchRequestsPage,
+  PropKeyValuePage,
+  UseEffectPage,
+  UpdateStatePage,
+  UseMemoPage,
+  UseCallbackPage,
+  ExhaustiveDepsPage
+} from './pages'
+
+function App() {
+  return (
+    <div className="container">
+      <h1>ReactJS - Bad practices</h1>
+      <p>Examples of bad practices in ReactJS applications</p>
+
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<HomePage />} />
+          <Route path="/multiple-states" element={<MultipleStatesPage />} />
+
+          <Route
+            path="/invoking-component-functions-directly"
+            element={<InvokingComponentFunctionsDirectlyPage />}
+          />
+
+          <Route
+            path="/component-within-components"
+            element={<ComponentWithinComponentsPage />}
+          />
+
+          <Route
+            path="/aborting-fetch-requests"
+            element={<AbortingFetchRequestsPage />}
+          />
+
+          <Route path="/prop-key-value" element={<PropKeyValuePage />} />
+          <Route path="/use-effect" element={<UseEffectPage />} />
+          <Route path="/update-state" element={<UpdateStatePage />} />
+          <Route path="/use-memo" element={<UseMemoPage />} />
+          <Route path="/use-callback" element={<UseCallbackPage />} />
+          <Route path="/exhaustive-deps" element={<ExhaustiveDepsPage />} />
+          <Route path="*" element={<NoMatchPage />} />
+        </Route>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
